846b4540dbb48e46986f40316b62ed82a70fab5a
ronshapiraweber <roon.shapira@gmail.com>
2019-10-27 21:42:20 +0200

    Fixed bug in argument parser

--- examples/UCR_alignment.py
+++ examples/UCR_alignment.py
@@ -19,7 +19,7 @@ import numpy as np
 from tensorflow.keras import backend as K
 
 # From helper
-from helper.util import get_dataset_info, print_NCC_results, get_test_results
+from helper.util import get_dataset_info
 from helper.plotting import plot_signals
 from helper.UCR_loader import load_UCR_data
 
@@ -31,9 +31,11 @@ def argparser():
     parser = argparse.ArgumentParser(description='Process args')
     parser.add_argument('--tess_size', type=int, default=16,
                         help="CPA velocity field partition")
-    parser.add_argument('--smoothness_prior', type=bool, default=True,
-                        help="smoothness prior flag")
-    parser.add_argument('--lambda_smooth', type=float, default=0.5,
+    parser.add_argument('--smoothness_prior', default=True,
+                        help="smoothness prior flag", action='store_true')
+    parser.add_argument('--no_smoothness_prior', dest='smoothness_prior', default=True,
+                        help="no smoothness prior flag", action='store_false')
+    parser.add_argument('--lambda_smooth', type=float, default=1,
                         help="lambda_smooth, larger values -> smoother warps")
     parser.add_argument('--lambda_var', type=float, default=0.1,
                         help="lambda_var, larger values -> larger warps")
@@ -46,10 +48,10 @@ def argparser():
 
 if __name__ == "__main__":
     args = argparser()
-
     # data
     datadir = "data/"
     dataset_name = "ECGFiveDays"
+
 
     X_train, X_test, y_train, y_test = load_UCR_data(datadir, dataset_name)
     print("y_train.shape", y_train.shape)
@@ -71,4 +73,4 @@ if __name__ == "__main__":
     # plot results
     if plot_signals_flag:
         # Plot test data
-        plot_signals(X_test, X_test_aligned, y_test, dataset_name=dataset_name)
+        plot_signals(X_test, X_test_aligned, y_test, ratio=[10,6], dataset_name=dataset_name)
